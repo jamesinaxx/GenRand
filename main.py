5ed0479be5279244188901841c2cb3d0754eba9c
jamesinaxx <grassojames5@gmail.com>
2022-02-05 15:19:52 +1100

    Fixes

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 from random import choice
-from sys import stdout, argv
+from sys import stdout, argv, exit
 from time import sleep
 
-letters = 'abcdefghijklmnopqrstuvwxyz'
+def main():
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    len = 4
 
-len = 4
+    if (argv.__len__() > 1):
+        len = int(argv[1])
 
-if (argv.__len__() > 1):
-    len = int(argv[1])
+    def rand_ls():
+        letterls = ''
 
-def rand_ls():
-    letterls = ''
+        for _ in range(0,len):
+            letterls = letterls + (choice(letters)) + ' '
 
-    for _ in range(0,len):
-        letterls = letterls + (choice(letters)) + ' '
+        return letterls
 
-    return letterls
+    while True:
+        stdout.write(f"\r{rand_ls()}\n")
 
-while True:
-    stdout.write(f"\r{rand_ls()}\n")
+        for _ in range(0,50):
+            stdout.write(f"\r{rand_ls()}")
+            stdout.flush()
+            try:
+                sleep(0.02)
+            except:
+                exit(0)
 
-    for _ in range(0,50):
-        stdout.write(f"\r{rand_ls()}")
-        stdout.flush()
-        try:
-            sleep(0.02)
-        except:
-            exit(0)
+try:
+    main()
+except KeyboardInterrupt:
+    exit(0)
